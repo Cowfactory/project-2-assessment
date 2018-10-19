947a257b282c59d43301a9b37069aa81231f6a81
Cowfactory <hhongnr@gmail.com>
2018-10-19 14:21:43 -0700

    finished

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.home, name='home'),
+    path('<int:pk>/delete/', views.WidgetDelete.as_view(), name='deleteWidget'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic.edit import DeleteView
+from .models import Widget
+from .forms import WidgetForm
 
 # Create your views here.
+
+def home(request):
+    widgets = Widget.objects.all()
+    totalQuantity = 0
+    for widget in widgets:
+        totalQuantity += widget.quantity
+
+    if request.method == 'POST':
+        form = WidgetForm(request.POST)
+        form.save()
+        return HttpResponseRedirect('/')
+
+    else:
+        form = WidgetForm()
+        return render(request, 'home.html', {'widgets': widgets, 'form': form, 'totalQuantity': totalQuantity})
+
+def deleteWidget(request, widget_id):
+    print(widget_id)
+
+class WidgetDelete(DeleteView):
+    model = Widget
+    success_url = "/" 
